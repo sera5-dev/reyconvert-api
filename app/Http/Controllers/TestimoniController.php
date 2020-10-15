c51c783f3da6d1840ee2881b053bba4702e6f9ef
rifqisambas <rifqi@sera5.id>
2020-10-16 04:29:04 +0700

    update HomeController | change middleware exception

--- app/Http/Controllers/TestimoniController.php
+++ app/Http/Controllers/TestimoniController.php
@@ -10,7 +10,7 @@ class TestimoniController extends Controller
 {
 	public function __construct()
 	{
-		$this->middleware('auth:api');
+		$this->middleware('auth:api', ['except' => ['index']]);
 	}
 
 	public function index()
