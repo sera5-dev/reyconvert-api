6a345f546b48fe81b1b0935a64b587e8063d3225
rifqisambas <rifqi@sera5.id>
2020-12-10 05:01:05 +0700

    update auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,84 +8,84 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-	public function __construct()
-	{
-		$this->middleware('auth:api', ['except' => ['login']]);
-	}
+  public function __construct()
+  {
+    $this->middleware('auth:api', ['except' => ['login', 'register']]);
+  }
 
-	public function register(Request $request)
-	{
-		$this->validate($request, [
-			'username' 	=> 'required|string|unique:users',
-			'email' 		=> 'required|string|unique:users',
-			'nama' 			=> 'required|string',
-			'password' 	=> 'required|confirmed',
-		]);
+  public function register(Request $request)
+  {
+    $this->validate($request, [
+      'username'   => 'required|string|unique:users',
+      'email'     => 'required|string|unique:users',
+      'nama'       => 'required|string',
+      'password'   => 'required|confirmed',
+    ]);
 
-		try {
-			$user = new User;
-			$user->nama 		= $request->input('nama');
-			$user->email 		= $request->input('email');
-			$user->username = $request->input('username');
-			$user->password = app('hash')->make($request->input('password'));
-			$user->save();
+    try {
+      $user = new User;
+      $user->nama     = $request->input('nama');
+      $user->email     = $request->input('email');
+      $user->username = $request->input('username');
+      $user->password = app('hash')->make($request->input('password'));
+      $user->save();
 
-			return response()->json([
-				'message' => 'users successfully created',
-			], 201);
-		} catch (\Exception $e) {
-			return response()->json([
-				'message' => 'create users failed',
-			], 409);
-		}
-	}
-	public function login(Request $request)
-	{
-		$this->validate($request, [
-			'username' => 'required|string',
-			'password' => 'required|string',
-		]);
+      return response()->json([
+        'message' => 'users successfully created',
+      ], 201);
+    } catch (\Exception $e) {
+      return response()->json([
+        'message' => 'create users failed',
+      ], 409);
+    }
+  }
+  public function login(Request $request)
+  {
+    $this->validate($request, [
+      'username' => 'required|string',
+      'password' => 'required|string',
+    ]);
 
-		$credentials = $request->only(['username', 'password']);
+    $credentials = $request->only(['username', 'password']);
 
-		if (!$token = Auth::attempt($credentials)) {
-			return response()->json([
-				'message' => 'login failed'
-			], 401);
-		}
-		return response()->json([
-			'token' => $token,
-		]);
-	}
-	public function logout()
-	{
-		try {
-			Auth::logout();
-			return response()->json([
-				'data' => null,
-				'message' => 'user successfully logged out'
-			], 201);
-		} catch (\Exception $e) {
-			return response()->json([
-				'message' => 'user logout failed',
-				'error' => $e,
-			], 409);
-		}
-	}
+    if (!$token = Auth::attempt($credentials)) {
+      return response()->json([
+        'message' => 'login failed'
+      ], 401);
+    }
+    return response()->json([
+      'token' => $token,
+    ]);
+  }
+  public function logout()
+  {
+    try {
+      Auth::logout();
+      return response()->json([
+        'data' => null,
+        'message' => 'user successfully logged out'
+      ], 201);
+    } catch (\Exception $e) {
+      return response()->json([
+        'message' => 'user logout failed',
+        'error' => $e,
+      ], 409);
+    }
+  }
 
-	public function unregister(Request $request)
-	{
-		try {
-			User::findOrFail($request->input('id'))->delete();
+  public function unregister(Request $request)
+  {
+    try {
+      User::findOrFail($request->input('id'))->delete();
 
-			return response()->json([
-				'message' => 'user successfully deleted',
-			]);
-		} catch (\Exception $e) {
-			return response()->json([
-				'message' => 'delete user failed',
-				'error' => $e
-			]);
-		}
-	}
+      return response()->json([
+        'message' => 'user successfully deleted',
+      ]);
+    } catch (\Exception $e) {
+      return response()->json([
+        'message' => 'delete user failed',
+        'error' => $e
+      ]);
+    }
+  }
 }
