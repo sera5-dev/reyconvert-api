a83942cdbd3bc97402a31b539bb52fc313b0347f
rifqisambas <rifqi@sera5.id>
2020-12-10 05:01:21 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,19 +3,27 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		$this->call([
-			ProviderSeeder::class,
-			RateSeeder::class,
-		]);
-	}
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call([
+      ProviderSeeder::class,
+      RateSeeder::class,
+    ]);
+
+    User::create([
+      'email' => 'admin@sera5.id',
+      'username' => 'admin',
+      'password' => md5('admin'),
+      'nama' => 'admin',
+    ]);
+  }
 }
